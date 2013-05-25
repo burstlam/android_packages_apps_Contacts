79972d9864fc2f41d26ed2f884732ef9758a92da
Danny Baumann <dannybaumann@web.de>
2013-05-25 11:59:25 +0200

    Fix a number of issues with T9 dialer.
    - Fix an ArrayIndexOutOfBoundsException (like reported in
      [1]) which happened when highlighting the last digits of a number and
      the number had additional characters after those digits.
    - Fix another potential ArrayIndexOutOfBoundsException due to the
      highlighting code relying on normalNumber being a subset of number,
      which wasn't the case due to number being additionally sent through a
      formatter. Fix that by running the formatter before normalizing.
    - Fix number highlighting not working if the number match occured at the
      end of the number.
    - Fix number type label & formatting not being updated on locale
      changes.
    - Fix T9 search not being triggered when pressing the dial button to
      recall the last dialed number.
    
    Change-Id: Iae1087cf1805fd2840cb6c740e763037d59bd564
    
    Conflicts:
    	src/com/android/contacts/dialpad/DialpadFragment.java

--- src/com/android/contacts/dialpad/DialpadFragment.java
+++ src/com/android/contacts/dialpad/DialpadFragment.java
@@ -1504,6 +1504,8 @@ public class DialpadFragment extends Fragment
                 // contains a *formatted* version of mLastNumberDialed (due to
                 // mTextWatcher) and its length may have changed.
                 mDigits.setSelection(mDigits.getText().length());
+
+                // Retrigger T9 search to show the matching contact if present
                 searchContacts();
             } else {
                 // There's no "last number dialed" or the
